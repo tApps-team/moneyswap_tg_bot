6b7c6ce06e32395322f7de7b6b3fccd1e88f1d01
Vlad <yashenkov.v@yandex.ru>
2024-05-31 14:24:00 +0300

    fix: edit start text

--- handlers.py
+++ handlers.py
@@ -12,26 +12,30 @@ from db.base import Base
 main_router = Router()
 
 start_text = '''
-💱 Добро пожаловать в MoneySwap!
-Наш бот поможет найти лучшую сделку под вашу задачу 💸
-Чтобы начать поиск, выберите категорию “безналичные”, “наличные” или “Swift/Sepa” и нажмите на нужную кнопку ниже, чтобы открыть мониторинг обменников.
-Если есть какие-то вопросы, обращайтесь [поддержка]. Мы всегда готовы вам помочь.
+💱 <b<Добро пожаловать в MoneySwap!</b><br/>
+
+Наш бот поможет найти лучшую сделку под вашу задачу 💸<br/>
+
+👉🏻 <b>Чтобы начать поиск</b>, выберите категорию “безналичные”, “наличные” или “Swift/Sepa” и нажмите на нужную кнопку ниже.<br/>
+
+Если есть какие-то вопросы, обращайтесь [поддержка]. Мы всегда готовы вам помочь!
 '''
 
 @main_router.message(Command('start'))
 async def start(message: types.Message,
                 session: Session):
-    Guests = Base.classes.general_models_guest
+    Guest = Base.classes.general_models_guest
     username = message.from_user.username
     tg_id = message.from_user.id
-    guest = session.query(Guests).where(Guests.tg_id == tg_id).all()
-    print(guest)
+    guest = session.query(Guest).where(Guest.tg_id == tg_id).all()
+    # print(guest)
     if not guest:
-        session.execute(insert(Guests).values(username=username,
+        session.execute(insert(Guest).values(username=username,
                                               tg_id=tg_id))
         session.commit()
     start_kb = create_start_keyboard()
     await message.answer(start_text,
+                         parse_mode='html',
                         reply_markup=start_kb.as_markup(resize_keyboard=True,
                                                         is_persistent=True))
     await message.delete()
