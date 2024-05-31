484a5bb69ef70452b4ffae076d43d23c214192c1
Vlad <yashenkov.v@yandex.ru>
2024-05-31 11:29:13 +0300

    fix: switch from inline buttons to keybord buttons

--- keyboards.py
+++ keyboards.py
@@ -1,17 +1,26 @@
 from aiogram import types
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder, KeyboardBuilder
 from aiogram.types.web_app_info import WebAppInfo
 from config import WEBAPP_URL_ONE, WEBAPP_URL_TWO, WEBAPP_URL_THREE
 
 
 
 def create_start_keyboard():
-    start_kb = InlineKeyboardBuilder()
-    start_kb.add(types.InlineKeyboardButton(text='Наличные',
-                                            web_app=WebAppInfo(url='https://www.bestexchanges.world/')))
-    start_kb.add(types.InlineKeyboardButton(text='Безналичные',
-                                            web_app=WebAppInfo(url='https://www.bestexchanges.world/')))
-    start_kb.row(types.InlineKeyboardButton(text='Swift/Sepa',
-                                            callback_data='3'))
+    start_kb = KeyboardBuilder()
+    # start_kb = InlineKeyboardBuilder()
+    start_kb.add(types.KeyboardButton(text='Наличные',
+                                      web_app=WebAppInfo(url='https://www.bestexchanges.world/')))
+
+    start_kb.add(types.KeyboardButton(text='Безналичные',
+                                      web_app=WebAppInfo(url='https://www.bestexchanges.world/')))
+
+    start_kb.row(types.KeyboardButton(text='Swift/Sepa'))
+
+    # start_kb.add(types.InlineKeyboardButton(text='Наличные',
+    #                                         web_app=WebAppInfo(url='https://www.bestexchanges.world/')))
+    # start_kb.add(types.InlineKeyboardButton(text='Безналичные',
+    #                                         web_app=WebAppInfo(url='https://www.bestexchanges.world/')))
+    # start_kb.row(types.InlineKeyboardButton(text='Swift/Sepa',
+    #                                         callback_data='3'))
     
     return start_kb
