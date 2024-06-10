73f03ca7251268eab399abdf48ad7706770afe8e
Vlad <yashenkov.v@yandex.ru>
2024-06-10 18:41:03 +0300

    fix: edit url param for keyboard buttons

--- keyboards.py
+++ keyboards.py
@@ -9,9 +9,9 @@ def create_start_keyboard(user_id: int):
     start_kb = ReplyKeyboardBuilder()
     
     start_kb.add(types.KeyboardButton(text='Безналичные',
-                                      web_app=WebAppInfo(url=f'https://www.bestexchanges.world/?direction=noncash?user_id={user_id}')))
+                                      web_app=WebAppInfo(url=f'https://www.bestexchanges.world/?direction=noncash&user_id={user_id}')))
     start_kb.add(types.KeyboardButton(text='Наличные',
-                                      web_app=WebAppInfo(url=f'https://www.bestexchanges.world/?direction=cash?user_id={user_id}')))
+                                      web_app=WebAppInfo(url=f'https://www.bestexchanges.world/?direction=cash&user_id={user_id}')))
     start_kb.row(types.KeyboardButton(text='Swift/Sepa'))
     
     return start_kb
