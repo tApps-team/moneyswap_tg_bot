b8655b1146fa7ef055f46c741abcbb1428929976
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-02-13 16:53:11 +0300

    feat: add language_code to web_app url in btns

--- utils/multilanguage.py
+++ utils/multilanguage.py
@@ -2,7 +2,7 @@
 
 
 start_text_dict = {
-    'ru': '💱<b>Добро пожаловать в MoneySwap!</b>\n\nНаш бот поможет найти лучшую сделку под вашу задачу 💸\n\n👉🏻 <b>Чтобы начать поиск</b>, выберите категорию “безналичные”, “наличные” или “Swift/Sepa” и нажмите на нужную кнопку ниже.\n\nЕсли есть какие-то вопросы, обращайтесь <a href="https://t.me/MoneySwap_support">Support</a> или <a href="https://t.me/moneyswap_admin">Admin</a>. Мы всегда готовы вам помочь!',
+    'ru': '💱<b>Добро пожаловать в MoneySwap!</b>\n\nНаш бот поможет найти лучшую сделку под вашу задачу 💸\n\n👉🏻 <b>Чтобы начать поиск</b>, выберите категорию “Безналичные”, “Наличные” или “Swift/Sepa” и нажмите на нужную кнопку ниже.\n\nЕсли есть какие-то вопросы, обращайтесь <a href="https://t.me/MoneySwap_support">Support</a> или <a href="https://t.me/moneyswap_admin">Admin</a>. Мы всегда готовы вам помочь!',
     'en': '💱<b>Welcome to MoneySwap!</b>\n\nOur bot will help you find the best deal for your exchange needs 💸\n\n👉🏻 <b>To start searching</b>, select a category: “Non-cash,” “Cash,” or “Swift/Sepa”, then click the appropriate button below.\n\nIf you have any questions, contact <a href="https://t.me/MoneySwap_support">Support</a> or <a href="https://t.me/moneyswap_admin">Admin</a>. We are always ready to help!',
 }
 
