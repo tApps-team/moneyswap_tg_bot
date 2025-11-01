97cc6b68267d558c25978b77fbe9e9bb4df0afd5
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-11-01 11:20:10 +0300

    fix: edit mass send massage

--- handlers.py
+++ handlers.py
@@ -1834,13 +1834,13 @@ async def send_mass_message(bot: Bot,
             # )
             
             #test for me only
-            query = (
-                select(Guest)\
-                .where(Guest.tg_id.in_([686339126]))
-            )
+            # query = (
+            #     select(Guest)\
+            #     .where(Guest.tg_id.in_([686339126]))
+            # )
 
             # mass_send for all guests
-            # query = (select(Guest))
+            query = (select(Guest))
 
             res = await _session.execute(query)
 
